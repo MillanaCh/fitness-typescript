579118fc20df7294ae787c0cf9fa0f36a496ac64
millana_chotubaeva <chotubaevam@gmail.com>
2023-03-23 16:09:09 -0700

    * done responsivness by adding hook and mobile screen with pop up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
+import { SelectedPage } from "./shared/types";
+
 function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   return (
     <div className="app bg-gray-20">
-      <Navbar />
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,7 +1,77 @@
-type Props = {};
+import { useEffect, useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
-const Navbar = (props: Props) => {
-  return <div>Navbar</div>;
+type Props = {
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  //represents certain classes, will apply this tailwind css classes
+  const flexBetween = "flex items-center justify-between";
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* LEFT SIDE */}
+            <img alt="logo" src={Logo} />
+
+            {/* RIGHT SIDE */}
+
+            {isAboveMediumScreens ? (
+              //DESKTOP
+              <div className={`${flexBetween} w-full`}>
+                <div className={`${flexBetween} gap-8 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <p>Sign In</p>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
+                </div>
+              </div>
+            ) : (
+              //MOBILE screen
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6, text-while w-6" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
