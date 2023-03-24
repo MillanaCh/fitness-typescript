7c6e13e6a6ec3b76f051f255d1ad5c278f542e5b
millana_chotubaeva <chotubaevam@gmail.com>
2023-03-24 11:21:24 -0700

    * add benefits page * create re-usable component * Benefits -> Benefit * Our class component  with overflow carousel of images * react-hook-form for contact us to reach email (form-submit) * add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import Navbar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
+import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
+import Classes from "@/scenes/ourClasses";
+import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -28,6 +33,11 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
+      <Classes setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
